a27c1d54588974e075c85dca30ff140560d0c1a8
Z-Vladus <z-vladus@bk.ru>
2022-03-27 23:12:26 +0300

    Java LVL2 Lesson2 homework

---
+++ src/ru/geekbrainsLev2/Lesson2/homework/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package ru.geekbrainsLev2.Lesson2.homework;
+
+public class MyArrayDataException extends Throwable {
+
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/ru/geekbrainsLev2/Lesson2/homework/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package ru.geekbrainsLev2.Lesson2.homework;
+
+public class MyArraySizeException extends Exception {
+
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
