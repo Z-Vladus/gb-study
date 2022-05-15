5c62c8b9efbf3678e7bd926e607d524e06d65c5c
Z-Vladus <z-vladus@bk.ru>
2022-05-16 02:05:06 +0300

    lesson 3-1 --

--- src/ru/GB/LVL3/Lesson1/Homework/Task1app.java
+++ src/ru/GB/LVL3/Lesson1/Homework/Task1app.java
@@ -3,12 +3,9 @@ package ru.GB.LVL3.Lesson1.Homework;
 public class Task1app {
 
     public static void main(String[] args) {
-        String str0= new String();
-        str0="str0";
-        String str1= new String();
-        str1="str1";
-        String str2= new String();
-        str2="str2";
+        String str0="str0";
+        String str1="str1";
+        String str2="str2";
         String[] strAr;
         strAr = new String[]{str0,str1,str2};
 
--- src/ru/GB/LVL3/Lesson1/Homework/Task2.java
+++ src/ru/GB/LVL3/Lesson1/Homework/Task2.java
@@ -7,8 +7,6 @@ public class Task2 {
     public static void main(String[] args) {
         int[] intArray = {1,2,3,4,5};
         ArrayList list;
-
-
     }
 
     public <T> ArrayList convertToArrayList(){
